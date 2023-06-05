db10d1b5ff3f225ce84b16b2cf99acd895980e86
devAlex101 <alejandrortsinani101@gmail.com>
2023-06-05 00:52:57 -0400

    primer version, modificaciones propias en espera

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,4 +15,12 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  overrides: [
+    {
+      files: ["pages/**/*.vue", "layouts/*.vue"],
+      rules: {
+        "vue/multi-word-component-names": "off",
+      },
+    },
+  ],
 };
